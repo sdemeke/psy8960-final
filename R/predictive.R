8f96104ed53ec134b36eb6ced5eabf2db637a40d
Saron M Demeke <demek004@umn.edu>
2023-08-28 10:33:00 -0500

    create predictive.R, load libraries for text processing

---
+++ R/predictive.R
@@ -0,0 +1,11 @@
+# Script Settings and Resources
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(tidyverse)
+library(tidytext)
+library(textdata)
+library(tm)
+library(qdap)
+library(textstem)
+library(RWeka)
+
+
