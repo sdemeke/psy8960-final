09390c568916fdd2928f887653adb4684083c074
Saron M Demeke <demek004@umn.edu>
2023-08-30 18:31:08 -0500

    done with output table, working on plots

--- shiny/people_dashboard/app.R
+++ shiny/people_dashboard/app.R
@@ -9,6 +9,7 @@
 
 library(shiny)
 library(tidyverse)
+library(jtools)
 
 #read in saved skinny data
 #final_shinydata <- read_rds("../shiny/people_dashboard/final_shiny_dat.RDS") #change to "./.rds" later
@@ -22,32 +23,46 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30),
-        
         #first selection input takes in user choice for outcome Default monthly pay selected
-        selectInput("outcome","Select Outcome",choices = c("Monthly Pay","Turnover Status","Overall Satisfaction"), selected = "Monthly Pay"),
+        selectInput("outcome","Select Outcome",choices = c("Monthly Pay" = "MonthlyIncome","Turnover Status"="Attrition_num","Overall Satisfaction"="JobSatisfaction"), selected = "Monthly Pay"),
         #first selection input takes in user choice for grouping variable. Default all cases shown (no group)
         #selectInput("group","Select Group",choices = c("Gender","Department","Education Field","Job Role","All"), selected = "All"),
         
-        checkboxGroupInput("group", "Select Group(s) to show:",
-                           c("Gender" = "Gender",
-                             "Department" = "Department",
-                             "Educational Field" = "Education Field",
-                             "Job Role" = "Job Role",
-                             "All")),
-       
+        # checkboxGroupInput("group", "Select Group(s) to show in summary table:",
+        #                    c("Gender" = "Gender",
+        #                      "Department" = "Department",
+        #                      "Educational Field" = "EducationField",
+        #                      "Job Role" = "JobRole",
+        #                      "All"
+        #                     ),
+        #                    selected = "Department"),
         
+        selectInput("gender", "Select gender groups to plot",
+                    choices = c("Female","Male", "All"),
+                    selected = "All"),
+        selectInput("department", "Which departments do you want to see in plot?",
+                    choices = c("Human Resources","Research & Development", "Sales","All"),
+                    selected = "All"),
+        selectInput("educ_field", "Which educational fields do you want to see in plot?",
+                    choices = c("Human Resources","Life Sciences", "Marketing","Medical","Other","Technical Degree","All"),
+                    selected = "All"),
+        selectInput("job_role", "Which job roles do you want to see in plot?",
+                    choices = c("Healthcare Representative","Human Resources", "Laboratory Technician","Manager",
+                                "Manufacturing Director" ,"Research Director" ,"Research Scientist" ,"Sales Executive" ,
+                                "Sales Representative", "All"),
+                    selected = "All"),
+        
+        
+        
+        
+
         ),
         
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
-           # ,
-           # tableOutput("data")
+          width = 8,
+           plotOutput("univariatePlot"),
+           tableOutput("means_sds_bygroup")
         )
     )
 )
@@ -56,65 +71,90 @@ ui <- fluidPage(
 server <- function(input, output) {
   
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+    # output$distPlot <- renderPlot({
+    #     # generate bins based on input$bins from ui.R
+    #     x    <- faithful[, 2]
+    #     bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    # 
+    #     # draw the histogram with the specified number of bins
+    #     hist(x, breaks = bins, col = 'darkgray', border = 'white',
+    #          xlab = 'Waiting time to next eruption (in mins)',
+    #          main = 'Histogram of waiting times')
+    # })
     
     #output plot
+  
+  output$univariatePlot <- renderPlot({
+    
+    if(input$outcome == "MonthlyIncome") {
+    final_shinydata %>% 
+      #filter data based on selected gender. Default/'All' choice results in everything selected
+      filter(if (input$gender!="All") Gender==input$gender else TRUE) %>%  
+      filter(if (input$department!="All") Department==input$department else TRUE) %>%  
+      filter(if (input$educ_field!="All") EducationField==input$educ_field else TRUE) %>%  
+      filter(if (input$job_role!="All") JobRole==input$job_role else TRUE) %>%  
+      ggplot(aes(x = .data[[input$outcome]])) +
+      geom_histogram(bins = 30, fill = "grey30") +
+      labs( title = "Distribution of Monthly",
+            x = "Monthly Pay",
+            
+        
+      ) +
+      theme_minimal()
+    }
+    
+    
+  })
+  
+  
     
     #output table - means and sds of selected outcome by group
+    
+    output$means_sds_bygroup <- renderTable({
+      
+ 
+      all_possible_cols <- c("Gender","Department","EducationField","JobRole")
+      selected_cols <- vector(mode="list",length = length(all_possible_cols))  
+      names(selected_cols) <- all_possible_cols
+      
+      selected_cols[["Gender"]] <- ifelse(input$gender %in% levels(final_shinydata$Gender), TRUE,FALSE)
+      selected_cols[["Department"]] <- ifelse(input$department %in% levels(final_shinydata$Department), TRUE,FALSE)
+      selected_cols[["EducationField"]] <- ifelse(input$educ_field %in% levels(final_shinydata$EducationField), TRUE,FALSE)
+      selected_cols[["JobRole"]] <- ifelse(input$job_role %in% levels(final_shinydata$JobRole), TRUE,FALSE)
+      
+      
+      cols_to_group <- all_possible_cols[unlist(selected_cols)]
+
+      if(length(cols_to_group) >= 1){
+      final_shinydata %>%
+        group_by(across(all_of(cols_to_group))) %>%
+        summarise("Mean" = mean(.data[[input$outcome]], na.rm = T),
+                  "SD" = sd(.data[[input$outcome]], na.rm = T)
+                  )
+      } else {
+      final_shinydata %>%
+          summarise("Mean" = mean(.data[[input$outcome]], na.rm = T),
+                    "SD" = sd(.data[[input$outcome]], na.rm = T)
+          )
+
+      }
+      
+
+
+    })
+    
+    
     
     
 }
 
+final_shinydata %>% 
+  filter(Department == "Human Resources") %>% 
+  ggplot(aes(x = MonthlyIncome)) +
+  geom_histogram(bins = 30, fill = "grey30") +
+  theme_minimal()
 
 
-## Only run examples in interactive R sessions
-# if (interactive()) {
-#   
-#   ui <- fluidPage(
-#     checkboxGroupInput("variable", "Variables to show:",
-#                        c("Cylinders" = "cyl",
-#                          "Transmission" = "am",
-#                          "Gears" = "gear")),
-#     tableOutput("data")
-#   )
-#   
-#   server <- function(input, output, session) {
-#     output$data <- renderTable({
-#       mtcars[, c("mpg", input$variable), drop = FALSE]
-#     }, rownames = TRUE)
-#   }
-#   
-#   shinyApp(ui, server)
-#   
-#   ui <- fluidPage(
-#     checkboxGroupInput("icons", "Choose icons:",
-#                        choiceNames =
-#                          list(icon("calendar"), icon("bed"),
-#                               icon("cog"), icon("bug")),
-#                        choiceValues =
-#                          list("calendar", "bed", "cog", "bug")
-#     ),
-#     textOutput("txt")
-#   )
-#   
-#   server <- function(input, output, session) {
-#     output$txt <- renderText({
-#       icons <- paste(input$icons, collapse = ", ")
-#       paste("You chose", icons)
-#     })
-#   }
-#   
-#   shinyApp(ui, server)
-# }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
