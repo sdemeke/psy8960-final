8ec74f49412e3cffff60256aa234f57ebc203ade
Saron M Demeke <demek004@umn.edu>
2023-08-30 01:05:00 -0500

    roughly done with H1

--- R/statistical.R
+++ R/statistical.R
@@ -9,3 +9,34 @@ library(rstatix)
 
 #Load combined data file from Part 1
 full_dat <- read_rds("../rds/combined_dataset.rds")
+
+### Visualization
+
+#make this more apa
+
+#H1 - Relationship between monthly pay and performance rating
+full_dat %>% ggplot(aes(x=MonthlyIncome, y=PerformanceRating)) +
+ # geom_point(position = "jitter", alpha = 0.7)
+  geom_point(position = position_jitter(width = 1,seed = 24)) +
+  geom_smooth(method = "lm", se = FALSE, color = "darkgrey") +
+  labs(
+    title = "Figure 1. Relationship Between Monthly Pay and Performance Ratings",
+    x = "Monthly Income",
+    y = "Performance Ratings"
+  ) +
+  theme_minimal() 
+
+### Analysis
+
+#H1 - Relationship between monthly pay and performance rating
+full_dat %>% 
+  cor_test(vars = c(MonthlyIncome, PerformanceRating), alternative = "two.sided")
+
+
+
+#H2 - Monthly pay differs by department
+
+
+#H3 - Tenure can be predicted from relationship satisfaction. This link is moderated by gender.
+
+
