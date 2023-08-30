85c356bca3d925d2e22c06479b2b35ee374395df
Saron M Demeke <demek004@umn.edu>
2023-08-30 12:54:21 -0500

    done with h2

--- R/statistical.R
+++ R/statistical.R
@@ -17,15 +17,17 @@ h1_test <- full_dat %>%
 
 
 
-#H2 - Monthly pay differs by department
+#Test of H2 - Monthly pay differs by department
+#detailed = T returns more info like SS
 h2_test <- full_dat %>% 
   anova_test(dv = MonthlyIncome, wid = EmployeeID, between = Department,
              detailed = T)
+#anova is significant. post hoc tukey test to compare group differences 
+h2_post_test <- full_dat %>% 
+  tukey_hsd(MonthlyIncome ~ Department)
 
 
-
-
-#H3 - Tenure can be predicted from relationship satisfaction. This link is moderated by gender.
+#Test of H3 - Tenure can be predicted from relationship satisfaction. This link is moderated by gender.
 h3_test <- lm(YearsAtCompany ~ RelationshipSatisfaction*Gender, data = full_dat)
 
 #use tidy() and glance() to make pretty
@@ -33,7 +35,6 @@ h3_test <- lm(YearsAtCompany ~ RelationshipSatisfaction*Gender, data = full_dat)
 
 ### Visualization
 
-#make this more apa
 
 #Visualization of H1 
 (ggplot(data = full_dat,
@@ -50,14 +51,22 @@ h3_test <- lm(YearsAtCompany ~ RelationshipSatisfaction*Gender, data = full_dat)
   ) %>%  
   ggsave(filename="../fig/H1.png", units="px", width=1920, height=1080)
 
+
 #Visualization of H2 - Monthly pay differs by department
-full_dat %>% ggplot(aes(x=Department, y=MonthlyIncome)) +
-  geom_boxplot()
+(ggplot(data = full_dat,
+        aes(x=Department, y=MonthlyIncome)) +
+  geom_boxplot() +
+  labs(
+    title = "Figure 2. Monthly Pay by Department",
+    x = "Department",
+    y = "Monthly Pay"
+  ) +
+  theme_apa() + #from {jtools}, removes gridlines, sets font sizes to align with APA
+  theme(plot.title = element_text(size = 12))  #title was too far to the right, cut off 
+) %>%  
+  ggsave(filename="../fig/H2.png", units="px", width=1920, height=1080)
 
 
-
-
-#add titles, anything else?
 
 #H3 - Tenure can be predicted from relationship satisfaction. This link is moderated by gender.
 
@@ -78,7 +87,7 @@ custom_decimal <-  function(x){
 
   }
 
-#Test of H1
+#H1
 #"The correlation between monthly pay and performance rating is r = -.50, p =.51. This test was not statistically significant."
 
 paste0("The correlation between monthly pay and performance rating is r = ",
@@ -90,5 +99,34 @@ paste0("The correlation between monthly pay and performance rating is r = ",
       )
 
 
+#H2
 
+#ANOVA summary table
+h2_aov_tbl <- tibble(
+  "Predictor" = c(h2_test$Effect, "Error"),
+  "Sum of Squares" = c(h2_test$SSn,h2_test$SSd),
+  "df" = c(h2_test$DFn,h2_test$DFd),
+  "Mean Square" = c(h2_test$SSn/h2_test$DFn,h2_test$SSd/h2_test$DFd),
+  "F" = c(custom_decimal(h2_test$`F`), " "),
+  "p" = c(custom_decimal(h2_test$p), " ")
+  
+) %>% 
+  write_csv("../out/H2.csv")
 
+#table of means and sds by department
+h2_desc_tbl <- full_dat %>% 
+  group_by(Department) %>% 
+  summarise("M" = mean(MonthlyIncome, na.rm = T), 
+            "SD" = sd(MonthlyIncome, na.rm = T))
+
+#text interpretation
+paste0("A one-way ANOVA was conducted to compare mean scores of monthly pay by departmental groups. The results in the summary table show that there is a significant difference in monthly income across departments, F(",
+       "df1 = ",h2_test$DFn, ", df2 = ",h2_test$DFd,") = ", custom_decimal(h2_test$`F`),
+       ", p = ", custom_decimal(h2_test$p),
+       ". A post hoc (Tukey HSD) test revealed a statistically significant difference between the Sales",
+       " (M = ", custom_decimal(h2_desc_tbl$M[3]),", SD = ", custom_decimal(h2_desc_tbl$SD[3]),
+       ") and Research & Development (M = ", custom_decimal(h2_desc_tbl$M[2]),", SD = ", custom_decimal(h2_desc_tbl$SD[2]),
+       ") departments. Monthly pay for Sales is higher than income in the R&D group (p = ", custom_decimal(h2_post_test$p.adj[3]), ")."
+       
+  
+)
