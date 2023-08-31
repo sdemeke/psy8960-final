f26128cf55f3d1a974987453ed8b79dd7310419f
Saron M Demeke <demek004@umn.edu>
2023-08-30 22:12:23 -0500

    finished part 1

--- R/data_import.R
+++ R/data_import.R
@@ -1,23 +1,24 @@
-#Script Settings and Resources
+### Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 
-#Data Import
+### Data Import and Cleaning
 
-#import dataset.csv
+#Import dataset.csv with correct delimeter
 dataset <- read_delim("../data/dataset.csv", 
                       delim = "+") %>% 
-  mutate(EmployeeID = 1:1470, .before = Age) %>% #add column specifying employee id
+  mutate(EmployeeID = 1:1470, .before = Age) %>% #add column creating sequential employee id
   mutate(across(where(is.character), as.factor)) %>% #change all character columns to factor
   mutate(across(c(Education,JobLevel,StockOptionLevel), as.factor)) #change numeric columns of categorical variables to factor
 
-#import satisfaction_review.csv
+#Import satisfaction_review.csv
 sat_revies <- read_delim("../data/satisfaction_reviews.csv",
-                         col_names = c("PosReview","NegReview","EmployeeID"), #name cols accordingly
+                         col_names = c("PosReview","NegReview","EmployeeID"), #name columns accordingly
                          delim = ".") 
 
-#combine datasets joining by id
+#Combine datasets joining by id
 full_dat <- left_join(dataset, sat_revies, by="EmployeeID")
 
-#save combined file to RDS
+
+#save combined file to my rds folder in project directory
 write_rds(full_dat, "../rds/combined_dataset.rds")
